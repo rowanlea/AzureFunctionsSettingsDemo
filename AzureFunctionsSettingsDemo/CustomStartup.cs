a186eb6392fcc14e82de4e2eef17449d5ef859dc
Rowan <rowan.lea92@gmail.com>
2023-04-23 20:05:17 +0100

    Backing up

---
+++ AzureFunctionsSettingsDemo/CustomStartup.cs
@@ -0,0 +1,38 @@
+﻿using Azure.Identity;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+[assembly: FunctionsStartup(typeof(MyNamespace.Startup))]
+
+namespace MyNamespace
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+   
+        }
+
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            // Build normal config so I can read key vault uri
+            var builtConfig = builder.ConfigurationBuilder.Build();
+
+            var keyVaultEndpoint = builtConfig["KeyVaultUri"];
+            var tenantId = builtConfig["tenantId"];
+            var clientId = builtConfig["clientId"];
+            var clientSecret = builtConfig["clientSecret"];
+            var multiplier = builtConfig["CustomMultiplier"];
+
+            var creds = new ClientSecretCredential(tenantId, clientId, clientSecret);
+
+            // Add everything - not sure if I need all
+            var final = builder.ConfigurationBuilder
+                .AddAzureKeyVault(new Uri(keyVaultEndpoint), creds)
+                .Build();
+            var multiplier2 = final["CustomMultiplier"];
+        }
+    }
+}
--- AzureFunctionsSettingsDemo/Function1.cs
+++ AzureFunctionsSettingsDemo/Function1.cs
@@ -1,22 +1,69 @@
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Threading.Tasks;
 
+
 namespace AzureFunctionsSettingsDemo
 {
-    public static class Multiply
+    public class Multiply
     {
+        private readonly IConfiguration _configuration = null;
+
+        public Multiply(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         [FunctionName("Multiply")]
-        public static async Task<IActionResult> Run(
+        public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req)
         {
             int baseMultiplier = 1;
-            
-            var fromSettings = Environment.GetEnvironmentVariable("CustomMultiplier");
+
+            var fromSettings = _configuration["CustomMultiplier"];
+
             Int32.TryParse(fromSettings, out baseMultiplier);
+
+            int number = Convert.ToInt32(req.Query["number"]);
+            return new OkObjectResult($"Multiplied number is: {number * baseMultiplier}");
+        }
+
+        [FunctionName("CheckKeyVault")]
+        public async Task<IActionResult> CheckKeyVault(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req)
+        {
+            int baseMultiplier = 1;
+
+            var keyVaultEndpoint = _configuration["KeyVaultUri"];
+            var tenantId = _configuration["tenantId"];
+            var clientId = _configuration["clientId"];
+            var clientSecret = _configuration["clientSecret"];
+
+            var creds = new ClientSecretCredential(tenantId, clientId, clientSecret);
+
+            var client = new SecretClient(vaultUri: new Uri(keyVaultEndpoint), credential: creds);
+            KeyVaultSecret secret = client.GetSecret("CustomMultiplier");
+            Int32.TryParse(secret.Value, out baseMultiplier);
+
+            int number = Convert.ToInt32(req.Query["number"]);
+            return new OkObjectResult($"Multiplied number is: {number * baseMultiplier}");
+        }
+
+        [FunctionName("Apps")]
+        public async Task<IActionResult> Apps(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req)
+        {
+            int baseMultiplier = 1;
+
+            var fromapps = Environment.GetEnvironmentVariable("CustomMultiplier");
+
+            Int32.TryParse(fromapps, out baseMultiplier);
 
             int number = Convert.ToInt32(req.Query["number"]);
             return new OkObjectResult($"Multiplied number is: {number * baseMultiplier}");
