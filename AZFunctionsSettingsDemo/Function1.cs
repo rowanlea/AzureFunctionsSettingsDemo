8fc9839e834ee96e04658a82b600d80b1e8f0774
Rowan <rowan.lea92@gmail.com>
2023-05-03 16:36:12 +0100

    Tidied up, with extra settings

--- AZFunctionsSettingsDemo/Function1.cs
+++ AZFunctionsSettingsDemo/Function1.cs
@@ -1,23 +1,41 @@
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Threading.Tasks;
 
 namespace AZFunctionsSettingsDemo
 {
-    public static class Function1
+    public class Function1
     {
+        private readonly IConfiguration _configuration = null;
+
+        public Function1(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         [FunctionName("Function1")]
-        public static async Task<IActionResult> Run(
+        public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req)
         {
             int baseMultiplier = 1;
 
-            var fromSettings = Environment.GetEnvironmentVariable("CustomMultiplier");
-            Int32.TryParse(fromSettings, out baseMultiplier);
+            var keyVaultEndpoint = _configuration["KeyVaultUri"];
+            var tenantId = _configuration["tenantId"];
+            var clientId = _configuration["clientId"];
+            var clientSecret = _configuration["clientSecret"];
+
+            var creds = new ClientSecretCredential(tenantId, clientId, clientSecret);
+
+            var client = new SecretClient(vaultUri: new Uri(keyVaultEndpoint), credential: creds);
+            KeyVaultSecret secret = client.GetSecret("CustomMultiplier");
+
+            Int32.TryParse(secret.Value, out baseMultiplier);
 
             int number = Convert.ToInt32(req.Query["number"]);
             return new OkObjectResult($"Multiplied number is: {number * baseMultiplier}");
---
+++ AZFunctionsSettingsDemo/Startup.cs
@@ -0,0 +1,34 @@
+﻿using Azure.Identity;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System;
+
+[assembly: FunctionsStartup(typeof(MyNamespace.Startup))]
+
+namespace MyNamespace
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+
+        }
+
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            // Build normal config so I can read key vault uri
+            var builtConfig = builder.ConfigurationBuilder.Build();
+
+            var keyVaultEndpoint = builtConfig["KeyVaultUri"];
+            var tenantId = builtConfig["tenantId"];
+            var clientId = builtConfig["clientId"];
+            var clientSecret = builtConfig["clientSecret"];
+
+            var creds = new ClientSecretCredential(tenantId, clientId, clientSecret);
+
+            builder.ConfigurationBuilder
+                .AddAzureKeyVault(new Uri(keyVaultEndpoint), creds)
+                .Build();
+        }
+    }
+}
